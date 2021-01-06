857eaf20143d54a814ba30c830b2d3f47163f136
776A0A <har3dy@icloud.com>
2021-01-06 22:07:49 +0800

    feat: 一些学习node的demo；

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const http = require('http')
+const minimist = require('minimist')
+const chalk = require('chalk')
+const ProgressBar = require('progress')
 
 const hostname = '127.0.0.1'
 const port = 3000
@@ -6,7 +9,7 @@ const port = 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 500
   res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World!')
+  res.end('Hello World!!!')
 })
 
 server.listen(port, hostname, () => {
